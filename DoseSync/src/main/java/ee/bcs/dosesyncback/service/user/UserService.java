d3e5036f2c946180f59090f13e43eca5f404087a
Apelsinka365 <olga.kuvatova@gmail.com>
2025-05-29 14:05:58 +0300

    added get /user to: UserController UserService UserRepository UserMapper

--- DoseSync/src/main/java/ee/bcs/dosesyncback/service/user/UserService.java
+++ DoseSync/src/main/java/ee/bcs/dosesyncback/service/user/UserService.java
@@ -1,6 +1,7 @@
 package ee.bcs.dosesyncback.service.user;
 
 import ee.bcs.dosesyncback.controller.user.dto.UserDto;
+import ee.bcs.dosesyncback.infrastructure.exception.PrimaryKeyNotFoundException;
 import ee.bcs.dosesyncback.persistence.user.User;
 import ee.bcs.dosesyncback.persistence.user.UserMapper;
 import ee.bcs.dosesyncback.persistence.user.UserRepository;
@@ -14,20 +15,32 @@ import java.util.List;
 @RequiredArgsConstructor
 public class UserService {
 
-private final UserRepository userRepository;
-private final UserMapper userMapper;
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
-public List<UserDto> getAllUsers(){
-List<User> users = userRepository.findAll();
-List<UserDto> userDtos = userMapper.toUserDtos(users);
-return userDtos;
+    public List<UserDto> getAllUsers() {
+        List<User> users = userRepository.findAll();
+        List<UserDto> userDtos = userMapper.toUserDtos(users);
+        return userDtos;
 
-}
+    }
 
-@Transactional
+    @Transactional
     public Integer createUser(UserDto userDto) {
-    User user = userMapper.toUser(userDto);
-    User savedUser = userRepository.save(user);
-    return savedUser.getId();
-}
+        User user = userMapper.toUser(userDto);
+        User savedUser = userRepository.save(user);
+        return savedUser.getId();
+    }
+
+    public UserDto findUser(Integer userId) {
+        User user = getValidUserBy(userId);
+        UserDto userDto = userMapper.toUserDto(user);
+        return userDto;
+    }
+
+    private User getValidUserBy(Integer userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new PrimaryKeyNotFoundException("userId", userId));
+        return user;
+    }
 }
